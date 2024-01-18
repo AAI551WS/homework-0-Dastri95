d660c96b8260db9d1b5b99b0192a74bb186f5980
Dastri95 <dkoto@stevens.edu>
2024-01-18 12:44:30 -0500

    HW 0 Submission 1

--- answer.py
+++ answer.py
@@ -1,21 +1,19 @@
 def hello_world():
+    # Write a doc string below. Doc strings can be used to provide the user with a
+    # description of what the function does.
     """
-    Prints 'Hello, World!' to the console.
+DocStrings were initially defined with PEP 257 as a way to describe the function of modules and functions. DocString
+syntax is three Quotation marks followed by text and closed with three Quotation marks.
+This simple script produces defines a function that prints "Hello World!" to console and then defines a function
+that adds two arguments a and b.
     """
-    # This is a simple print statement to display a greeting message
-    print("Hello, World!")
 
-def add(a, b):
-    """
-    Adds two input values and returns the result.
+    # Write a comment; comments are very useful to other programmers.
+# The following portion of this script prints the string "Hello World!" when the function hello_world() is called.
+    # Write a Python statement to print Hello, World!
+    print("Hello World!")
 
-    Parameters:
-    a (int or float): The first operand.
-    b (int or float): The second operand.
-
-    Returns:
-    int or float: The result of adding a and b.
-    """
-    # Simple code to add variable a and b
+def add(a,b):
+    # Write simple code to add variable a and b
     output = a + b
     return output
